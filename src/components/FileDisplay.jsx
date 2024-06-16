d1ae8ade598d5a048aa827760814ac30b154adb1
Sameer <sameersattar1111@gmail.com>
2024-06-17 04:07:40 +0500

    start project

---
+++ src/components/FileDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FileDisplay = () => {
+  return (
+    <div>FileDisplay</div>
+  )
+}
+
+export default FileDisplay
---
+++ src/components/Transcribing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Transcribing = () => {
+  return (
+    <div>Transcribing</div>
+  )
+}
+
+export default Transcribing
